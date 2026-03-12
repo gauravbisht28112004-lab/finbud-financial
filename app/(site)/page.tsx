74950d9c1a57ed161362c3a88779d0ae1282c719
gauravbisht28112004-lab <gauravbisht28112004@gmail.com>
2026-03-12 18:15:01 +0530

    Fix ContactSection prop error

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -98,7 +98,7 @@ export default async function HomePage() {
       </ScrollReveal>
 
       <ScrollReveal>
-        <ContactSection settings={s} />
+        <ContactSection />
       </ScrollReveal>
 
     </>
